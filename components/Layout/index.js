786607211b3be0a251b504c75970db13b0a5b071
Nishit Jha <jhanishitback@gmail.com>
2022-11-07 14:25:11 +0530

    ok

---
+++ components/Layout/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import styles from './index.module.scss';
+import { useRouter } from 'next/router';
+
+export default function Layout({ children, page }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>{page.title + ` • Startup Valley`}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=VT323:wght@200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <>
+        <div className={styles.main}>
+          <div className={styles.content}>{children}</div>
+        </div>
+      </>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,16 @@ import ProtectedRoute from '../components/ProtectedRoute';
 import styles from '../styles/Home.module.scss';
 import { useSession, signOut } from 'next-auth/react';
 import PrimaryButton from '../components/Buttons/PrimaryButton';
+import Layout from '../components/Layout';
+import Content from '../modules/Home';
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <ProtectedRoute>
-      <div className={styles.container}>
-        {session && (
-          <div>
-            <h1>hello {session.user.name}</h1>
-            <Image
-              src={session.user.image}
-              alt='Picture of the author'
-              width={500}
-              height={500}
-            />
-
-            <h2>email: {session.user.email}</h2>
-
-            <h2>expires at: {session.expires}</h2>
-
-            <PrimaryButton>Sign Out</PrimaryButton>
-          </div>
-        )}
-      </div>
+      <Layout page={{ title: 'Home' }}>
+        <Content />
+      </Layout>
     </ProtectedRoute>
   );
 }
