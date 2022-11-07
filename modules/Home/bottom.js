c9f6938f173b0d4b34b199c6752ae9c76cb138df
Arnav Bansal <arnavbansal101@gmail.com>
2022-11-07 11:23:37 +0530

    complete landing

--- modules/Home/bottom.js
+++ modules/Home/bottom.js
@@ -1,12 +1,33 @@
 import styles from "./bottom.module.scss";
+import Plx from "react-plx";
 
 const Bottom = () => {
   return (
     <div className={styles.bottom}>
-      <p className={styles.text}>(scroll further)</p>
-      <p className={styles.text}>skip</p>
+      <Plx
+        parallaxData={[
+          {
+            start: `700vh`,
+            end: `725vh`,
+            properties: [{ startValue: 1, endValue: 0, property: "opacity" }],
+          },
+        ]}
+      >
+        <p className={styles.text}>(scroll further)</p>
+      </Plx>
+      <p
+        className={styles.text}
+        onClick={() => {
+          window.scrollTo({
+            top: window.innerHeight * 7,
+            behavior: "smooth",
+          });
+        }}
+      >
+        skip
+      </p>
     </div>
   );
 };
 
-export default Bottom
+export default Bottom;
--- modules/Home/index.js
+++ modules/Home/index.js
@@ -1,12 +1,21 @@
 import styles from "./index.module.scss";
 import Nav from "./nav";
 import Bottom from "./bottom";
+import Main from "./main";
 
 export default function Content() {
   return (
     <div className={styles.main}>
       <Nav />
       <Bottom />
+      <Main />
+      <div className={styles.child} />
+      <div className={styles.child} />
+      <div className={styles.child} />
+      <div className={styles.child} />
+      <div className={styles.child} />
+      <div className={styles.child} />
+      <div className={styles.child} />
     </div>
   );
 }
