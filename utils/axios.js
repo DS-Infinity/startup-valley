ddfd8a1cffb99f93142fcb6379821a73f594235a
Nishit Jha <jhanishitback@gmail.com>
2022-11-09 00:10:29 +0530

    OK OK

--- utils/axios.js
+++ utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
     ? process.env.NEXT_PUBLIC_API_URL
-    : 'http://localhost:3000',
+    : '/',
   withCredentials: true,
 });
 
